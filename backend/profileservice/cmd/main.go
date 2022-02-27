85cb2ecfedb6cb0d6cc956f51633357b871b2ef3
omerbeden <omerbeden@gmail.com>
2022-02-27 13:20:54 +0300

    Edit sh & test repo

--- backend/profileservice/cmd/main.go
+++ backend/profileservice/cmd/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/omerbeden/event-mate/backend/profileservice/core"
+	"github.com/omerbeden/event-mate/backend/profileservice/infrastructure/database"
+	"github.com/omerbeden/event-mate/backend/profileservice/infrastructure/repositories"
+)
+
 func main() {
+	// test code
+	//TODO refeactor later
+	database.InitMigration(&core.User{}, &core.UserAdress{})
+
+	repo := repositories.NewUserRepo()
+
+	user := &core.User{
+		Name:     "omer",
+		LastName: "beden",
+		About:    "about about",
+		Adress: core.UserAdress{
+			City: "Sakarya",
+		},
+	}
+	fmt.Printf("%v", user.Model)
+
+	repo.InsertUser(user)
+
+	users := repo.GetUsers()
+	fmt.Printf("%+v", users)
 
 }
--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,13 +1,9 @@
 $docker create network --name event-mate-network
 
 
-$docker run -d --rm -p 5432:5432 --network event-mate-network postgres -h some-postgres -U postgres \
-     -e POSTGRES_PASSWORD=mysecretpassword \
+$docker run --rm -it --name dev-postgres -p 5432:5432 --network event-mate-network -e POSTGRES_PASSWORD=password -e POSTGRES_DB=test postgres
 
-$docker run -d --rm -p 80:80 --network event-mate-network \
-    -e 'PGADMIN_DEFAULT_EMAIL=user@domain.com' \
-    -e 'PGADMIN_DEFAULT_PASSWORD=SuperSecret' \
-    -e 'PGADMIN_CONFIG_ENHANCED_COOKIE_PROTECTION=True' \
-    -e 'PGADMIN_CONFIG_LOGIN_BANNER="Authorised users only!"' \
-    -e 'PGADMIN_CONFIG_CONSOLE_LOG_LEVEL=10' \
-    -d dpage/pgadmin4
+$docker run --rm -it --name dev-pgadmin  -p 80:80 --network event-mate-network -e 'PGADMIN_DEFAULT_EMAIL=user@domain.com' -e 'PGADMIN_DEFAULT_PASSWORD=SuperSecret' -e 'PGADMIN_CONFIG_LOGIN_BANNER="Authorised users only!"' -e 'PGADMIN_CONFIG_CONSOLE_LOG_LEVEL=10'  dpage/pgadmin4
+
+
+docker run --rm -it --name dev-postgres -e POSTGRES_DB=test -e POSTGRES_PASSWORD=Pass2020! -p 5432:5432 postgres
