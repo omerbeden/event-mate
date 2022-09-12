2094bba78d13bf60c02bee4bf3014f6dcb8cce11
omerbeden <omerbeden@gmail.com>
2022-09-12 14:16:19 +0300

    remove unnecessary

--- backend/eventservice/scripts/genproto.sh
+++
@@ -1,5 +0,0 @@
-
-protodir=../../../proto/grpc/event/v1
-
-protoc --go_out=plugins=grpc:genproto -I $protodir $protodir/event-service.proto
-protoc --go_out=plugins=grpc:genproto -I proto/grpc/event/v1/ proto/grpc/event/v1/event-service.proto
