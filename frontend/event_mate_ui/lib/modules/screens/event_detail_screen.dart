5bc9eebeaae3fb05ed2b0c64637e2accca19dd6d
omerbeden <omerbeden@gmail.com>
2023-01-29 18:10:15 +0300

    Add event detail screen

--- frontend/event_mate_ui/lib/modules/screens/event_detail_screen.dart
+++ frontend/event_mate_ui/lib/modules/screens/event_detail_screen.dart
@@ -1,86 +1,63 @@
+import 'package:event_mate/widgets/event_card_widget.dart';
 import 'package:flutter/material.dart';
 
-import '../../widgets/animatedsongcard.dart';
-
-/// Page shown when a card in the songs tab is tapped.
-///
-/// On Android, this page sits at the top of your app. On iOS, this page is on
-/// top of the songs tab's content but is below the tab bar itself.
-class EventDetailScreen extends StatelessWidget {
+class EventDetailScreen extends StatefulWidget {
   const EventDetailScreen({
     required this.id,
     Key? key,
   }) : super(key: key);
 
   final int id;
+  @override
+  State<StatefulWidget> createState() => EventDetailScreenState();
+}
 
-  Widget _buildBody() {
-    return SafeArea(
-      bottom: false,
-      left: false,
-      right: false,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Hero(
-            tag: id,
-            child: HeroAnimatingSongCard(
-              song: 'song',
-              color: Colors.red,
-              heroAnimation: const AlwaysStoppedAnimation(1),
-            ),
-            // This app uses a flightShuttleBuilder to specify the exact widget
-            // to build while the hero transition is mid-flight.
-            //
-            // It could either be specified here or in SongsTab.
-            flightShuttleBuilder: (context, animation, flightDirection,
-                fromHeroContext, toHeroContext) {
-              return HeroAnimatingSongCard(
-                song: 'song',
-                color: Colors.amber,
-                heroAnimation: animation,
-              );
-            },
-          ),
-          const Divider(
-            height: 0,
-            color: Colors.grey,
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                if (index == 0) {
-                  return const Padding(
-                    padding: EdgeInsets.only(left: 15, top: 16, bottom: 8),
-                    child: Text(
-                      'You might also like:',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  );
-                }
-                // Just a bunch of boxes that simulates loading song choices.
-                return const SongPlaceholderTile();
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildAndroid(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('song')),
-      body: _buildBody(),
-    );
+class EventDetailScreenState extends State<EventDetailScreen> {
+  Future<bool> onBackPress() {
+    return Future.value(false);
   }
 
   @override
   Widget build(BuildContext context) {
-    return _buildBody();
+    return Scaffold(
+        body: Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 10,
+            ),
+            child: Column(
+              // ignore: prefer_const_literals_to_create_immutables
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const EventCard(
+                    title: "title",
+                    description: "description",
+                    location: "location",
+                    duration: "duration"),
+                Text("Rules"),
+                Wrap(
+                  alignment: WrapAlignment.spaceAround,
+                  children: [
+                    for (var rule in [1, 2, 3]) Chip(label: Text("rule")),
+                  ],
+                ),
+                //ChatSection
+                Container(
+                    child: SafeArea(
+                  child: WillPopScope(
+                    child: Stack(
+                      children: [
+                        Column(
+                          children: [
+                            Text("list messages"),
+                            Text("input message")
+                          ],
+                        )
+                      ],
+                    ),
+                    onWillPop: onBackPress,
+                  ),
+                ))
+              ],
+            )));
   }
 }
