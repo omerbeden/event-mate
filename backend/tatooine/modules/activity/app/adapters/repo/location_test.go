b9224ca2f257a6b4530831ddbf9f094fe5cec974
omerbeden <omerbeden@gmail.com>
2024-02-27 15:51:10 +0300

    activity - add location repo test

--- backend/tatooine/modules/activity/app/adapters/repo/location_test.go
+++ backend/tatooine/modules/activity/app/adapters/repo/location_test.go
@@ -2,53 +2,131 @@ package repo_test
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"testing"
 	"time"
 
-	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/omerbeden/event-mate/backend/tatooine/modules/activity/app/adapters/repo"
 	"github.com/omerbeden/event-mate/backend/tatooine/modules/activity/app/domain/model"
-	postgres "github.com/omerbeden/event-mate/backend/tatooine/pkg/database"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateLocation(t *testing.T) {
-	dbConfig := postgres.PostgresConfig{
-		ConnectionString: "postgres://postgres:password@localhost:5432/test",
-		Config:           pgxpool.Config{MinConns: 5, MaxConns: 10},
+	tests := []struct {
+		name        string
+		id          int
+		location    model.Location
+		setupMock   func(*MockDBExecuter)
+		expectError bool
+	}{
+		{
+			name: "should create location",
+			id:   1,
+			location: model.Location{
+				City: "London",
+			},
+			expectError: false,
+			setupMock: func(md *MockDBExecuter) {
+				md.ExecFunc = func(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error) {
+					return pgconn.NewCommandTag(""), nil
+				}
+			},
+		},
+		{
+			name: "should return error",
+			id:   2,
+			location: model.Location{
+				City: "London",
+			},
+			expectError: true,
+			setupMock: func(md *MockDBExecuter) {
+				md.ExecFunc = func(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error) {
+					return pgconn.NewCommandTag(""), errors.New("database error")
+				}
+			},
+		},
 	}
-	pool := postgres.NewConn(&dbConfig)
-	repository := repo.NewLocationRepo(pool)
-	defer repository.Close()
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancel()
 
-	loc := model.Location{
-		ActivityId: 1,
-		City:       "Sakarya",
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("%s,%d", tc.name, tc.id), func(t *testing.T) {
+			mockDB := new(MockDBExecuter)
+			repo := repo.NewLocationRepo(mockDB)
+
+			if tc.setupMock != nil {
+				tc.setupMock(mockDB)
+			}
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+			defer cancel()
+
+			res, err := repo.Create(ctx, &tc.location)
+
+			if tc.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.True(t, res)
+			}
+		})
 	}
-
-	res, err := repository.Create(ctx, &loc)
-	assert.NotNil(t, res)
-	assert.NoError(t, err)
 }
+
 func TestUpdateLocation(t *testing.T) {
-	dbConfig := postgres.PostgresConfig{
-		ConnectionString: "postgres://postgres:password@localhost:5432/test",
-		Config:           pgxpool.Config{MinConns: 5, MaxConns: 10},
+	tests := []struct {
+		name        string
+		id          int
+		location    model.Location
+		setupMock   func(*MockDBExecuter)
+		expectError bool
+	}{
+		{
+			name: "should create location",
+			id:   1,
+			location: model.Location{
+				City: "London",
+			},
+			expectError: false,
+			setupMock: func(md *MockDBExecuter) {
+				md.ExecFunc = func(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error) {
+					return pgconn.NewCommandTag(""), nil
+				}
+			},
+		},
+		{
+			name: "should return error",
+			id:   2,
+			location: model.Location{
+				City: "NEW York",
+			},
+			expectError: true,
+			setupMock: func(md *MockDBExecuter) {
+				md.ExecFunc = func(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error) {
+					return pgconn.NewCommandTag(""), errors.New("database error")
+				}
+			},
+		},
 	}
-	pool := postgres.NewConn(&dbConfig)
 
-	repository := repo.NewLocationRepo(pool)
-	defer repository.Close()
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancel()
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("%s,%d", tc.name, tc.id), func(t *testing.T) {
+			mockDB := new(MockDBExecuter)
+			repo := repo.NewLocationRepo(mockDB)
 
-	locationToBeUpdated := model.Location{
-		ActivityId: 1,
-		City:       "Istanbul",
+			if tc.setupMock != nil {
+				tc.setupMock(mockDB)
+			}
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+			defer cancel()
+
+			res, err := repo.UpdateByID(ctx, tc.location)
+
+			if tc.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.True(t, res)
+			}
+		})
 	}
-	res, err := repository.UpdateByID(ctx, locationToBeUpdated)
-	assert.NotNil(t, res)
-	assert.NoError(t, err)
 }
