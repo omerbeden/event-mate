5637c7004843bd505d5ddc4dbf4e0f332ba66973
omerbeden <omerbeden@gmail.com>
2024-02-28 23:19:56 +0300

    activity - remove backgorund image field

--- backend/tatooine/modules/activity/app/domain/model/activity.go
+++ backend/tatooine/modules/activity/app/domain/model/activity.go
@@ -3,19 +3,18 @@ package model
 import "time"
 
 type Activity struct {
-	ID                 int64     `json:"id"`
-	Title              string    `json:"title"`
-	Category           string    `json:"category"`
-	CreatedBy          User      `json:"createdBy"`
-	Location           Location  `json:"location"`
-	Participants       []User    `json:"participants"`
-	BackgroundImageUrl string    `json:"backgroundImage"`
-	StartAt            time.Time `json:"startAt"`
-	EndAt              time.Time `json:"endAt"`
-	Content            string    `json:"content"`
-	Rules              []string  `json:"rules"`
-	Flow               []string  `json:"flow"`
-	Quota              int       `json:"quota"`
+	ID           int64     `json:"id"`
+	Title        string    `json:"title"`
+	Category     string    `json:"category"`
+	CreatedBy    User      `json:"createdBy"`
+	Location     Location  `json:"location"`
+	Participants []User    `json:"participants"`
+	StartAt      time.Time `json:"startAt"`
+	EndAt        time.Time `json:"endAt"`
+	Content      string    `json:"content"`
+	Rules        []string  `json:"rules"`
+	Flow         []string  `json:"flow"`
+	Quota        int       `json:"quota"`
 }
 
 type Location struct {
